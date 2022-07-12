6b8a7732b2b947e17f4571c8461aa95d6a1f13e8
Zac Youngdale <zac.youngdale@gmail.com>
2022-07-12 01:33:16 -0400

    fix ignor git in search

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -13,7 +13,7 @@ telescope.setup {
     prompt_prefix = " ",
     selection_caret = "  ",
     path_display = { "smart" },
-    file_ignore_patterns = { ".git/", "node_modules" },
+    file_ignore_patterns = { ".git", "node_modules" },
 
     mappings = {
       i = {
